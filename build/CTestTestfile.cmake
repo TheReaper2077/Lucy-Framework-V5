57a37ec72a1960cbe8fd796fb38f327aa9cc058c
TheReaper2077 <krishnadev7777@gmail.com>
2022-07-09 18:23:03 +0530

    structures_

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/C++/Lucy Framework V5
-# Build directory: D:/C++/Lucy Framework V5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
